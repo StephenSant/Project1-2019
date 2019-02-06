28e929d27adaba4b48120f41bef9cc0827102570
SootyForge <42379545+SootyForge@users.noreply.github.com>
2019-02-06 14:54:23 +1100

    Player Inputs
    I'll be trying to get the UI and keybindings working in a neat way.

---
+++ 3D Platformer 2019/Assets/Scripts/Systems/PlayerInput.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// PlayerInput makes it possible to use rebound keys.
+public class PlayerInput : MonoBehaviour
+{
+    #region Variables
+    // Format is: public [SCRIPT NAME HERE] var;
+    public UncurledMovement ;
+    #endregion
+}
