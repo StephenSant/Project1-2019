b7b9e5ceef64d0ebf66bd81882f6cd6a0c6d6174
SootyForge <42379545+SootyForge@users.noreply.github.com>
2019-02-06 15:11:33 +1100

    001 - PlayerInput.cs
    Got the foundations setup. Can't test currently until MenuHandler.cs exists. Working on that next.

--- 3D Platformer 2019/Assets/Scripts/Systems/PlayerInput.cs
+++ 3D Platformer 2019/Assets/Scripts/Systems/PlayerInput.cs
@@ -5,8 +5,50 @@ using UnityEngine;
 // PlayerInput makes it possible to use rebound keys.
 public class PlayerInput : MonoBehaviour
 {
+    // Nothing but references to scripts which give/use keys.
     #region Variables
     // Format is: public [SCRIPT NAME HERE] var;
-    public UncurledMovement ;
+    public MenuHandler key;
+    public UncurledMovement uncurlController;
+    public CurledMovement curlController;
+    #endregion
+
+    // Where we get our control scripts.
+    #region -void Start()
+    // Start is called just before any of the Update methods is called the first time
+    private void Start()
+    {
+        // Get our MenuHandler's saved keybindings, and...
+        key = FindObjectOfType<MenuHandler>();
+
+        // ... the relevant functions within the scripts.
+        uncurlController = FindObjectOfType<UncurledMovement>();
+        curlController = FindObjectOfType<CurledMovement>();
+    }
+    #endregion
+
+    // Where we check our key inputs and execute our functions.
+    #region void Update()
+    // Update is called every frame, if the MonoBehaviour is enabled
+    void Update()
+    {
+        /// ATTENTION! CHECK COMPATIBILITY WITH MOVEMENT SCRIPTS!
+        #region Movement
+        // Ternary operator (courtesy of Manny); it's a new (fake) axis using the saved keys.
+        float inputH = Input.GetKey(key.right) ? 1f : Input.GetKey(key.left) ? -1f : 0;
+        float inputV = Input.GetKey(key.forward) ? 1f : Input.GetKey(key.backward) ? -1f : 0;
+
+        // Execute 'CharacterMovement.Move()' from here.
+        uncurlController.Move(inputH, inputV);
+
+        // Same as above, but for 'UncurledMovement.Jump()'.
+        if (Input.GetKeyDown(key.jump))
+        {
+            uncurlController.Jump();
+        }
+
+        // controller.UpdateController();
+        #endregion
+    }
     #endregion
 }
