64e41d85bbab9e94317e468bdca0788a2907b4f1
Stephen <42423953+StephenSant@users.noreply.github.com>
2019-02-06 16:02:05 +1100

    Curled up done

--- 3D Platformer 2019/Assets/Scripts/UncurledMovement.cs
+++ 3D Platformer 2019/Assets/Scripts/UncurledMovement.cs
@@ -2,26 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class UncurledMovement : MonoBehaviour
 {
-    public float moveSpeed;
-    public float jumpHeight;
+    public float moveSpeed=5;
+    public float jumpHeight=3;
 
     public bool grounded;
 
     private Rigidbody rigidbody;
+    private Transform parent;
 
     // Use this for initialization
     void Start()
     {
-       rigidbody = GetComponent<Rigidbody>();
+       //Remember to get everything from the parent
+       rigidbody = GameObject.FindWithTag("Player").GetComponentInParent<Rigidbody>();
+       parent = GameObject.FindWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
-        transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed);
+        parent.Translate(Vector3.forward * Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed);
+        parent.Translate(Vector3.right * Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed);
         Ray ray = new Ray(transform.position, Vector3.down);
         if (Physics.Raycast(ray, 1.1f))
         {
@@ -34,11 +37,6 @@ public class PlayerMovement : MonoBehaviour
         if (grounded && Input.GetButton("Jump"))
         {
             rigidbody.velocity = ((Vector3.up*100) * Time.deltaTime * jumpHeight);
-        }
-        if (transform.position.y <= -5)
-        {
-            transform.position = Vector3.zero;
-            rigidbody.velocity = Vector3.zero;
         }
     }
 }
