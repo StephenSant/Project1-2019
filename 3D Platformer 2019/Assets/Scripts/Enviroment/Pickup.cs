b8bef3dad8c4480e037f94dce46d67947bd67c8c
Sooty Forge <jetttay@hotmail.com>
2019-02-22 14:09:24 +1100

    022 - Pickup Script
    Meow.

---
+++ 3D Platformer 2019/Assets/Scripts/Enviroment/Pickup.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    #region Variables
+    public bool isTrap = false;
+    [Header("Components")]
+    public MeshRenderer mesh;
+    public Light glow;
+    #endregion
+
+    #region Functions 'n' Methods
+
+    // Where we get our components.
+    #region void Start()
+    // Start is called just before any of the Update methods is called the first time
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        glow = GetComponentInChildren<Light>();
+    }
+    #endregion
+
+    // Where we wait for the player to take the bait.
+    #region void OnTriggerEnter(Collider col)
+    // OnTriggerEnter is called when the Collider other enters the trigger
+    void OnTriggerEnter(Collider col)
+    {
+        // When the player touches it, activate the trap (and disable the object's rendering stuff).
+        if (col.tag == "Player" && isTrap == false)
+        {
+            isTrap = true;
+            mesh.enabled = false;
+            glow.enabled = false;
+            Debug.Log("Trap!");
+        }
+    }  
+    #endregion
+    #endregion
+}
